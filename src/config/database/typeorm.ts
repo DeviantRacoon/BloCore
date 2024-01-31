59ef20717e58ce5e94c260918887b8883864ff8c
Hector Zavala <hector.zavala@innovacionescomputacionalesvl.com>
2024-01-31 13:53:39 -0700

    [ Avances ]

--- src/config/database/typeorm.ts
+++ src/config/database/typeorm.ts
@@ -2,11 +2,12 @@ import { DataSource } from "typeorm";
 
 // Core
 import { PersonEntity } from "../../modules/core/domain/entities/person.entity";
+import { UserEntity } from "../../modules/core/domain/entities/user.entity";
 
 // Academic Management
-import { StudentEntity } from "../../modules/academic-management/domain/entities/student.entity";
-import { TeacherEntity } from "../../modules/academic-management/domain/entities/teacher.entity";
-import { SubjectEntity } from "../../modules/academic-management/domain/entities/subject.entity";
+// import { StudentEntity } from "../../modules/academic-management/domain/entities/student.entity";
+// import { TeacherEntity } from "../../modules/academic-management/domain/entities/teacher.entity";
+// import { SubjectEntity } from "../../modules/academic-management/domain/entities/subject.entity";
 
 export const AppDataSource = new DataSource({
     type: "mariadb",
@@ -15,13 +16,11 @@ export const AppDataSource = new DataSource({
     username: process.env.USER_MYSQL,
     password: process.env.PASSWORD_MYSQL,
     database: process.env.DATABASE_MYSQL,
-    // synchronize: true,
+    synchronize: true,
     // logging: true,
     entities: [
         PersonEntity,
-        StudentEntity,
-        TeacherEntity,
-        SubjectEntity,
+        UserEntity,
     ],
     subscribers: [],
     migrations: [],
--- src/config/middleware/routing.middleware.ts
+++ src/config/middleware/routing.middleware.ts
@@ -1,11 +1,11 @@
-import studentRouting from "../../modules/academic-management/infrastructure/res/routes/student.routes";
-import teacherRouting from "../../modules/academic-management/infrastructure/res/routes/teacher.routes";
-import subjectRouting from "../../modules/academic-management/infrastructure/res/routes/subject.routes";
+// import studentRouting from "../../modules/academic-management/infrastructure/res/routes/student.routes";
+// import teacherRouting from "../../modules/academic-management/infrastructure/res/routes/teacher.routes";
+// import subjectRouting from "../../modules/academic-management/infrastructure/res/routes/subject.routes";
 
 export const routingModule = (server: any) => {
-  server.app.use("/student", studentRouting);
-  server.app.use("/teacher", teacherRouting);
-  server.app.use("/subject", subjectRouting);
+  // server.app.use("/student", studentRouting);
+  // server.app.use("/teacher", teacherRouting);
+  // server.app.use("/subject", subjectRouting);
 
   return (req: any, response: any, next: any) => {
     next();
